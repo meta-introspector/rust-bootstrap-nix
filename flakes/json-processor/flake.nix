836bd0cb28b8bf94b11f4da1cb6ddcf7e00caff2
mike <h@solfunmeme.com>
2025-10-17 15:28:43 +0000

    update lock

--- flakes/json-processor/flake.nix
+++ flakes/json-processor/flake.nix
@@ -4,14 +4,17 @@
   inputs = {
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
     # Reference the rust-bootstrap-nix repository
-    rustBootstrapNix = {
-      url = "github:meta-introspector/rust-bootstrap-nix?ref=bootstrap-001";
-    };
+    
+    rustBootstrapNix.url = "github:meta-introspector/rust-bootstrap-nix?ref=8bbbceb41f915448d6ad2a81baa1c61802c6f90f";
+#    rustBootstrapNix.url = "github:meta-introspector/rust-bootstrap-nix?ref=feature/bootstrap-001";
+      #url = "github:meta-introspector/rust-bootstrap-nix?ref=";
+    #};
     # Reference the main Rust source code
-    rustSrc = {
-      url = "path:../../../../"; # Relative path from this flake to the main Rust source code
-      flake = false; # Treat it as a plain path
-    };
+   rustSrc.url = "github:meta-introspector/rust?ref=e6c1b92d0abaa3f64032d6662cbcde980c826ff2";
+#    rustSrc = {
+#      url = "path:../../../../"; # Relative path from this flake to the main Rust source code
+#      flake = false; # Treat it as a plain path
+#    };
     # Reference the evaluate-rust flake
     evaluateRustFlake = {
       url = "path:../evaluate-rust"; # Relative path from this flake to evaluate-rust flake
@@ -34,6 +37,9 @@
           jsonContent = builtins.readFile "${jsonOutputContent}/${filename}";
         in
         builtins.fromJSON jsonContent;
+
+      # Parse all JSON files
+      parsedJsons = builtins.map readAndParseJson jsonFiles;
 
       # Parse all JSON files and evaluate commands
       evaluatedPackages = builtins.concatLists (
