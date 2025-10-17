198e54b15f7e0fd8a022035b934bbeb34f65516f
mike <h@solfunmeme.com>
2025-10-17 22:47:30 +0000

    wip

--- flakes/bootstrap-builder/flake.nix
+++ flakes/bootstrap-builder/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
     rust-overlay.url = "github:meta-introspector/rust-overlay?ref=feature/CRQ-016-nixify";
     rust-src = {
-      url = "github:rust-lang/rust/archive/d772ccdfd1905e93362ba045f66dad7e2ccd469b.tar.gz";
+      url = "github:meta-introspector/rust?ref=d772ccdfd1905e93362ba045f66dad7e2ccd469b";
       flake = false;
     };
   };
@@ -22,9 +22,16 @@
         pname = "bootstrap";
         version = "0.1.0";
 
-        src = "${rust-src}/src/bootstrap";
+        src = rust-src; # Change src to the root of rust-src
 
-        cargoLock.lockFile = "${rust-src}/src/bootstrap/Cargo.lock";
+        # cargoLock.lockFile = "${rust-src}/src/bootstrap/Cargo.lock";
+        cargoHash = "";
+
+        # The cargo build command needs to be run from the src/bootstrap directory
+        # So we will add a preBuild phase to change directory
+        preBuild = ''
+          cd src/bootstrap
+        '';
       };
     };
 }
