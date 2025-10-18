cf8572cd4274bd0b14190a070cea738466a79bc8
mike <h@solfunmeme.com>
2025-10-18 22:43:56 +0000

    feat: Directly import Cargo.nix and pass all arguments

--- standalonex/src/flake.nix
+++ standalonex/src/flake.nix
@@ -4,26 +4,45 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-    cargo2nix.url = "github:cargo2nix/cargo2nix/v0.12.0";
   };
 
-  outputs = { self, nixpkgs, flake-utils, cargo2nix }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cargo2nix.overlays.default ];
+        pkgs = nixpkgs.legacyPackages.${system};
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+        # Arguments for Cargo.nix
+        cargoNixArgs = {
+          inherit pkgs;
+          rustPackages = rustToolchain;
+          buildRustPackages = rustToolchain; # Assuming same for now
+          hostPlatform = pkgs.stdenv.hostPlatform;
+          lib = pkgs.lib;
+          # These are usually provided by cargo2nix's internal functions
+          # We need to find a way to get them or define them.
+          # For now, let's try to pass dummy values or find them in pkgs.rustPlatform
+          mkRustCrate = pkgs.rustPlatform.buildRustPackage; # This is a guess
+          rustLib = pkgs.rustPlatform; # This is a guess
+          workspaceSrc = ./.; # Current directory as workspace source
+          ignoreLockHash = false; # Or true if we want to ignore
+          cargoConfig = { };
+          release = true; # Default value
+          rootFeatures = [ "bootstrap/default" "build_helper/default" ]; # Default value
+          hostPlatformCpu = null;
+          hostPlatformFeatures = [ ];
+          target = null;
+          codegenOpts = null;
+          profileOpts = null;
+          cargoUnstableFlags = null;
+          rustcLinkFlags = null;
+          rustcBuildFlags = null;
         };
 
-        rustVersion = "1.75.0"; # Or a specific version like "1.75.0"
+        cargoNix = import ./Cargo.nix cargoNixArgs;
 
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          inherit rustVersion;
-          packageFun = import ./Cargo.nix;
-        };
-
-        bootstrapApp = rustPkgs.workspace.bootstrap;
-        buildHelperApp = rustPkgs.workspace.build_helper;
+        bootstrapApp = cargoNix.workspace.bootstrap;
+        buildHelperApp = cargoNix.workspace.build_helper;
       in
       {
         packages = {
@@ -34,7 +53,7 @@
 
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.rust-bin.stable.${rustVersion}.default
+            rustToolchain
             pkgs.cargo
             pkgs.rustc
             pkgs.rustfmt
@@ -45,7 +64,7 @@
             pkgs.libiconv # For macOS
           ];
           CARGO_HOME = "${pkgs.writeText "cargo-home" ""}"; # Prevent cargo from writing to ~/.cargo
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
         };
       });
 }
