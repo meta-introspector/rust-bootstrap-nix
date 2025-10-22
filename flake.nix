938c50a3407eca77885ebf35b5fa3c7550b1b8f6
mike <h@solfunmeme.com>
2025-10-22 16:39:17 +0000

    fix: Direct config.toml output to /data/data/com.termux.nix/files/home/pick-up-nix2/vendor/rust/platform-tools-agave-rust-solana/vendor/rust-src/vendor/rust/rust-bootstrap-nix/outputs/out in Nix build (formatted)

--- flake.nix
+++ flake.nix
@@ -170,10 +170,9 @@
       packages.aarch64-linux.generatedConfigToml = pkgs_aarch64.runCommand "config.toml"
         {
           nativeBuildInputs = [ configuration-nix.packages.aarch64-linux.default pkgs_aarch64.nix ];
-          CONFIG_OUTPUT_PATH = "$TMPDIR/config.toml"; # Write to a temporary file in the build directory
+          CONFIG_OUTPUT_PATH = "$out"; # Write directly to the output path
         } ''
         ${configuration-nix.packages.aarch64-linux.default}/bin/configuration-nix
-        mv $CONFIG_OUTPUT_PATH $out
       '';
       apps.aarch64-linux.generateConfig = configuration-nix.apps.aarch64-linux.default;
 
@@ -181,10 +180,9 @@
       packages.x86_64-linux.generatedConfigToml = pkgs_x86_64.runCommand "config.toml"
         {
           nativeBuildInputs = [ configuration-nix.packages.x86_64-linux.default pkgs_x86_64.nix ];
-          CONFIG_OUTPUT_PATH = "$TMPDIR/config.toml"; # Write to a temporary file in the build directory
+          CONFIG_OUTPUT_PATH = "$out"; # Write directly to the output path
         } ''
         ${configuration-nix.packages.x86_64-linux.default}/bin/configuration-nix
-        mv $CONFIG_OUTPUT_PATH $out
       '';
       apps.x86_64-linux.generateConfig = configuration-nix.apps.x86_64-linux.default;
     };
