9b1b8b4d345a3e708e2e36597f68a255bde46efd
mike <h@solfunmeme.com>
2025-10-22 12:06:02 +0000

    Revert: Restore deleted file parse_inner_stage0.rs

---
+++ standalonex/src/bootstrap/src/core/config_utils/src/parse_inner_stage0.rs
@@ -0,0 +1,5 @@
+use crate::parsed_config::ParsedConfig;
+use crate::local_toml_config::LocalTomlConfig;
+pub fn parse_inner_stage0(config: &mut ParsedConfig, toml: &LocalTomlConfig) {
+    // Removed build_helper dependency
+}
