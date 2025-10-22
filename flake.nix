e06e6bc301dee699759f3a30e6f1adf760685a4d
mike <h@solfunmeme.com>
2025-10-22 16:41:59 +0000

    debug: Modify generatedConfigToml to create dummy config.toml (formatted)

--- flake.nix
+++ flake.nix
@@ -172,7 +172,7 @@
           nativeBuildInputs = [ configuration-nix.packages.aarch64-linux.default pkgs_aarch64.nix ];
           CONFIG_OUTPUT_PATH = "$out"; # Write directly to the output path
         } ''
-        ${configuration-nix.packages.aarch64-linux.default}/bin/configuration-nix
+        echo "Dummy content" > $out
       '';
       apps.aarch64-linux.generateConfig = configuration-nix.apps.aarch64-linux.default;
 
@@ -182,7 +182,7 @@
           nativeBuildInputs = [ configuration-nix.packages.x86_64-linux.default pkgs_x86_64.nix ];
           CONFIG_OUTPUT_PATH = "$out"; # Write directly to the output path
         } ''
-        ${configuration-nix.packages.x86_64-linux.default}/bin/configuration-nix
+        echo "Dummy content" > $out
       '';
       apps.x86_64-linux.generateConfig = configuration-nix.apps.x86_64-linux.default;
     };
