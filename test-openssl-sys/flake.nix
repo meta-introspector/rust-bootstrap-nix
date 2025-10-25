8110c82c044e58f00508f336a90bb48a60fb0a2e
mike <h@solfunmeme.com>
2025-10-25 15:53:48 +0000

    feat: Add test-openssl-sys crate and update flake.nix for common deps

---
+++ test-openssl-sys/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A standalone Rust test crate using openssl-sys";
+
+  inputs = {
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+    rust-overlay.url = "github:meta-introspector/rust-overlay?ref=feature/CRQ-016-nixify";
+    commonDepsFlake.url = "github:meta-introspector/rust-bootstrap-nix?ref=feature/CRQ-016-nixify&dir=nix/rust-deps";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, commonDepsFlake, ... }@inputs:
+    let
+      system = "aarch64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+      lib = nixpkgs.lib;
+      commonRustDeps = commonDepsFlake.common-rust-deps { inherit pkgs lib; };
+    in
+    {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "test-openssl-sys";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        buildInputs = commonRustDeps.commonBuildInputs;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (pkgs.rust-bin.stable.latest.default)
+        ] ++ commonRustDeps.commonBuildInputs;
+        shellHook = ''
+          export PKG_CONFIG_PATH="${commonRustDeps.pkgConfigPath}:$PKG_CONFIG_PATH"
+        '';
+      };
+    };
+}
